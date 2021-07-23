ce7827e495366c6c1c23cf648a4f9ef1f14605d8
Ioan <ioan-mihail.macovei@student.tuiasi.ro>
2021-07-23 10:25:27 +0300

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Yes</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="index.css"/>
+    </head>
+    <body>
+        <div class="main">
+        </div>
+    </body>
+</html>
